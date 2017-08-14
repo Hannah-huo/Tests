8fb0836442a6968b1e55c1a03d52315982d4033e
Helen Huo <huochenhui1008@163.com>
2017-08-14 16:03:44 +0800

    test0813, median number

---
+++ test0813/median_number.js
@@ -0,0 +1,16 @@
+/*
+* 给定一个未排序的整数数组，找到其中位数。
+function XXX(arr){}
+* */
+function medianNumber(arr){
+    arr = arr.sort();
+    let n = arr.length;
+    if(n % 2 === 0){
+        return ((arr[n/2 - 1] + arr[n / 2]) / 2);
+    }
+    else{
+        return (arr[(n-1)/2]);
+    }
+}
+let arr = [1,3,4,5];
+console.log(medianNumber(arr));
