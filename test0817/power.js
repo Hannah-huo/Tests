b2df8ea9329e6e13911c5c36352c63d27cba8077
Helen Huo <huochenhui1008@163.com>
2017-08-17 14:07:34 +0800

    test0817, square number

---
+++ test0817/power.js
@@ -0,0 +1,14 @@
+/*
+* 为数组 arr 中的每个元素求二次方。不要直接修改数组 arr，结果返回新的数组
+例如：arr = [5, 3, 7, 1]; 输出：[25, 9, 49, 1]
+要求：不要使用for，while循环
+* */
+
+function squareNumber(arr){
+    return arr.map(function (item) {
+        return item * item;
+        
+    })
+}
+
+console.log(squareNumber([5, 3, 7, 1]));
